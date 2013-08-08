e8737e4f15870d7bdb1d64bee62586975b34dc33
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-08 11:19:31 -0700

    Ran rails g Site index to generate site controller and pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Datamap::Application.routes.draw do
+  get "site/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
