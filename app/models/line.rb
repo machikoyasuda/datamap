6e4edd5084060ee79b1c7890ff09c33d66fa7995
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-09 10:34:29 -0700

    Fetch GTFS Metro Los Angeles routes, filter and select only Rail. Save Lines with name and number into database.

--- app/models/line.rb
+++ app/models/line.rb
@@ -3,6 +3,22 @@ class Line
   many :stations
 
   key :name, String
-  key :number, Integer
+  key :number, String
 
+  def self.load_lines
+    source = GTFS::Source.build("http://media.metro.net/developer/gtfs/google_transit.zip", {strict: false} )
+    lines = Array.new
+
+    # Filter and select Metro Rail only
+    lines = source.routes.select {|routes| routes.id =~/^80/}
+    # Returns @id, @long_name, @type, @color, @text_color
+
+    # Save each line with name and number attributes
+    lines.each do |line|
+        @line = self.new
+        @line.name   = line.long_name
+        @line.number = line.id
+        @line.save
+      end
+  end
 end
