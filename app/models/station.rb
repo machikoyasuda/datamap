97189c93a42004aede1739c9747b14a99c744fc3
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-09 10:48:46 -0700

    Fetch GTFS Metro Los Angeles stops, filter and select only Rail station. Save Stations with name, number, location lat and long, status into database.

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,9 +4,32 @@ class Station
   many :datapoints
 
   key :name, String
-  key :number, Integer
-  key :status, Boolean
-  key :established_at, Date
+  key :number, String
+
   key :location, Hash
 
+  key :status, Boolean
+  key :established_at, Date
+
+  def self.load_stations
+    source = GTFS::Source.build("http://media.metro.net/developer/gtfs/google_transit.zip", {strict: false})
+    stations = Array.new
+
+    # Filter and select Metro Rail stops, first mention only
+    # TODO: Test ^8\d{4}S
+    stations = source.stops.select {|stops| stops.id=~/^8\d{4}$/}
+    # Returns @id, @code, @name, @lat, @lon, @location_type, @parent_station
+
+    # Save each station with name and number attributes
+    stations.each do |station|
+        @station = self.new
+        @station.name   = station.name
+        @station.number = station.id
+        @station.location = {:lat => station.lat, :lon => station.lon}
+        @station.status = true
+        @station.save
+      end
+  end
+
+  # TODO: Add established_at
 end
