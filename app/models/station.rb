b8049ad561c506f5952668a10d43376875b3e301
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-12 17:28:41 -0700

    Add get_fips method to find Census block for station latitude and longitude. Load_stations will now load station and also FIPS

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,36 +1,46 @@
 class Station
   include MongoMapper::Document
+  require 'open-uri'
 
   many :datapoints
 
   key :name, String
   key :number, String
 
-  key :location, Hash
+  key :lat, Float
+  key :lon, Float
 
   key :status, Boolean
   key :established_at, Date
+
+  key :fips, String
+
+  def self.get_fips(lat, lon)
+    url = "http://data.fcc.gov/api/block/2010/find?format=json&latitude=#{lat}&longitude=#{lon}&showall=false"
+    JSON.parse(open(url).read)["Block"]["FIPS"]
+  end
 
   def self.load_stations
     source = GTFS::Source.build("http://media.metro.net/developer/gtfs/google_transit.zip", {strict: false})
     stations = Array.new
 
-    # Filter and select Metro Rail stops, first mention only
-    # TODO: Test ^8\d{4}S
+    # Add hash to array; Filter and select Metro Rail stops, first mention only
+    # TODO: Test ^8\d{4}Ss
     stations = source.stops.select {|stops| stops.id=~/^8\d{4}$/}
     # Returns @id, @code, @name, @lat, @lon, @location_type, @parent_station
 
     # Save each station with name and number attributes
-    # Convert @lat @lon to Hash key:value pair and convert to float
     stations.each do |station|
-        @station = self.new
+        @station        = Station.new
         @station.name   = station.name
         @station.number = station.id
-        @station.location = {:lat => station.lat.to_f, :lon => station.lon.to_f}
+        @station.lat    = station.lat.to_f
+        @station.lon    = station.lon.to_f
+        @station.fips  = get_fips(station.lat, station.lon)
         @station.status = true
         @station.save
       end
   end
   # TODO: Does this run every time I load the website? Or just once?
-  # TODO: Add established_at
+  # TODO LATER: Add established_at
 end
