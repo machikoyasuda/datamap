1fd129d8128572f0ee9133d9ebad03ae7f0dd410
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-16 15:57:49 -0700

    Refactor data retrieval to retrieve by Line name.

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -5,14 +5,12 @@ class LineController < ApplicationController
     @stations = Array.new
     @data = Hash.new
 
-    # Find each station, put name and datapoint into Hash, add to Array
-    Station.find_each(:number => /(804)(.*)/) do |x|
+    # Find a Line and all of its stations, put name and datapoint into Hash, add to Array
+    Line.find_by_name('Gold Line').stations.each do |x|
       @data = { "station" => x.name, "datapoint" => x.datapoints[0].point}
       @stations << @data
     end
 
-    # TODO: Need to DRY RegEx for station lines
-    # TODO: Need to DRY datapoints[0]
     # Write code to load JSON of the Line with params[:id]
     render json: stats
   end
