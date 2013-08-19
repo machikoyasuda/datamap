1a542eab38066dd9cf5ba5959433d37a39b7aa27
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-19 11:44:14 -0700

    Add about page.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,9 @@ class SiteController < ApplicationController
   def index
     # Lines
     @lines = Line.all
-
   end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Datamap::Application.routes.draw do
   # go to root, at site#index
   root 'site#index'
 
+  # get about page
+  get '/about',   to: 'site#about'
+
   # return JSON of line param[:id] data
   get 'line/:id/stats' => 'line#stats', defaults: { format: :json }
 end
