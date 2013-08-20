ef4ae528c222441b2f8d6372c0a11d147752990c
Machiko Yasuda <machikoyasuda@gmail.com>
2013-08-20 10:37:29 -0700

    Add download page of source data.

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -4,14 +4,32 @@ class LineController < ApplicationController
     @stations = Array.new
     @data = Hash.new
 
-    # TODO: Put AJAX clicked id into find_by_name
     # Find a Line and all of its stations, format data into Hash, add to Array
-    Line.find_by_id(params[:id]).stations.each do |x|
-      @data = { "station" => x.name, "datapoint" => x.datapoints[0].point}
+    Line.find_by_id(params[:id]).stations.each do |station|
+      @data = { "station" => station.name, "datapoint" => station.datapoints[0].point}
       @stations << @data
     end
 
     # Render JSON for AJAX request
+    render json: @stations
+  end
+
+  def download
+    require 'csv'
+
+    @lines = Line.all
+    @stations = Array.new
+    @data = Hash.new
+
+    # Create JSON
+    @lines.each do |line|
+      line.stations.each do |station|
+        @data = { "line" => line.name, "station" => station.name, "datapoint" => station.datapoints[0].point}
+        @stations << @data
+      end
+    end
+
+    # Render JSON
     render json: @stations
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Datamap::Application.routes.draw do
   root 'site#index'
 
   # get about page
-  get '/about',   to: 'site#about'
+  get '/about',     to: 'site#about'
+
+  # get download page
+  get '/download',  to: 'line#download'
 
   # return JSON of line param[:id] data
   get 'line/:id/stats' => 'line#stats', defaults: { format: :json }
